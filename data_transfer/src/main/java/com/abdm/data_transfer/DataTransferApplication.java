08dadb37a236d3f8bab83eb13b7bffb79bd50986
Aman Gadadare <93522857+AmanGadadare@users.noreply.github.com>
2023-09-05 00:58:22 +0530

    Add files via upload

---
+++ data_transfer/src/main/java/com/abdm/data_transfer/DataTransferApplication.java
@@ -0,0 +1,13 @@
+package com.abdm.data_transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataTransferApplication.class, args);
+	}
+
+}
